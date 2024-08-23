8323d6c38763607e2b7d7bf8175365204ace3915
writeland <2664040792@qq.com>
2024-08-23 21:39:05 +0800

    demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="main"></div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      option: {
+        legend: {},
+        tooltip: {},
+        dataset: {
+          source: [
+            ['product', '2012', '2013', '2014', '2015'],
+            ['Matcha Latte', 41.1, 30.4, 65.1, 53.3],
+            ['Milk Tea', 86.5, 92.1, 85.7, 83.1],
+            ['Cheese Cocoa', 24.1, 67.2, 79.5, 86.4]
+          ]
+        },
+        xAxis: [
+          { type: 'category', gridIndex: 0 },
+          { type: 'category', gridIndex: 1 }
+        ],
+        yAxis: [{ gridIndex: 0 }, { gridIndex: 1 }],
+        grid: [{ bottom: '55%' }, { top: '55%' }],
+        series: [
+          // 这几个系列会出现在第一个直角坐标系中，每个系列对应到 dataset 的每一行。
+          { type: 'bar', seriesLayoutBy: 'row' },
+          { type: 'bar', seriesLayoutBy: 'row' },
+          { type: 'bar', seriesLayoutBy: 'row' },
+          // 这几个系列会出现在第二个直角坐标系中，每个系列对应到 dataset 的每一列。
+          { type: 'bar', xAxisIndex: 1, yAxisIndex: 1 },
+          { type: 'bar', xAxisIndex: 1, yAxisIndex: 1 },
+          { type: 'bar', xAxisIndex: 1, yAxisIndex: 1 },
+          { type: 'bar', xAxisIndex: 1, yAxisIndex: 1 }
+        ]
+      }
+    }
+  },
+  methods: {
+    myEchart() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = this.$echarts.init(document.getElementById('main'))
+      // 绘制图表
+      myChart.setOption(this.option)
+    }
+  },
+  mounted() {
+    this.myEchart()
+  }
+}
+</script>
+
+<style scoped>
+#main {
+  width: 600px;
+  height: 400px;
+  margin: 0 auto;
+  /* background-color: pink; */
+}
+</style>
